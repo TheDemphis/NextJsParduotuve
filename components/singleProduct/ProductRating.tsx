2556b33c2cce7961d1df71e5f5fb395e6dc66621
TheDemphis <danielius.drevinskas@gmail.com>
2025-05-21 05:27:45 +0300

    Reviews pataisyti

--- components/singleProduct/ProductRating.tsx
+++ components/singleProduct/ProductRating.tsx
@@ -2,7 +2,9 @@ import { fetchProdRating } from '@/utils/actions'
 import { FaStar } from 'react-icons/fa'
 async function ProductRating({ productId }: { productId: string }) {
   const { rating, count } = await fetchProdRating(productId)
-  const reviews = `iš ${count} ${count > 1 ? 'atsiliepimų' : 'atsiliepimo'}`
+  const reviews = `iš ${count} ${
+    count === 0 ? 'atsiliepimų' : count > 1 ? 'atsiliepimų' : 'atsiliepimo'
+  }`
   const className = 'flex items-center mt-1  mb-3 gap-1'
   return (
     <span className={className}>
